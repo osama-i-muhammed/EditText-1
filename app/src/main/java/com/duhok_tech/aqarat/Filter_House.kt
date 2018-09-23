e04610d243c112b5a9317895b533e4ef6944ccab
Salar Pro <salarkhalid97@yahoo.com>
2018-09-24 02:01:48 +0300

    add Toast for testing

--- app/src/main/java/com/duhok_tech/aqarat/Filter_House.kt
+++ app/src/main/java/com/duhok_tech/aqarat/Filter_House.kt
@@ -2,6 +2,7 @@ package com.duhok_tech.aqarat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class Filter_House : AppCompatActivity() {
 
@@ -12,5 +13,7 @@ class Filter_House : AppCompatActivity() {
         //WWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
 
         var s=23+21+12
+
+        Toast.makeText(this,s.toString()+"ddd ",Toast.LENGTH_LONG).show()
     }
 }
