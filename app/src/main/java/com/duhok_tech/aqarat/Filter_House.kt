d724c09e8c78a4cacaa6dced0182e9bb38a47fa2
Salar Pro <salarkhalid97@yahoo.com>
2018-09-24 01:50:54 +0300

    New One

--- app/src/main/java/com/duhok_tech/aqarat/Filter_House.kt
+++ app/src/main/java/com/duhok_tech/aqarat/Filter_House.kt
@@ -8,5 +8,6 @@ class Filter_House : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter__house)
+        //Helo World
     }
 }
