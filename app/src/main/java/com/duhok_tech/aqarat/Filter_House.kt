3c3f1f2ac47005fcdabad3ae8e38a9db2a7c0198
Salar Pro <salarkhalid97@yahoo.com>
2018-09-24 02:02:57 +0300

    delete extra things

--- app/src/main/java/com/duhok_tech/aqarat/Filter_House.kt
+++ app/src/main/java/com/duhok_tech/aqarat/Filter_House.kt
@@ -9,11 +9,6 @@ class Filter_House : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter__house)
-        //Helo World
-        //WWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
 
-        var s=23+21+12
-
-        Toast.makeText(this,s.toString()+"ddd ",Toast.LENGTH_LONG).show()
     }
 }
