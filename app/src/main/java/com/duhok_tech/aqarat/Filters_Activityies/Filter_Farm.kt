4bec79030d1fa0e9d388bed51536158403c1d443
osama-i-muhammed <zduhoki93@gmail.com>
2018-09-24 18:10:01 +0300

    dsf

--- app/src/main/java/com/duhok_tech/aqarat/Filters_Activityies/Filter_Farm.kt
+++ app/src/main/java/com/duhok_tech/aqarat/Filters_Activityies/Filter_Farm.kt
@@ -13,9 +13,9 @@ class Filter_Farm : AppCompatActivity() {
         setContentView(R.layout.activity_filter__farm)
 
         //check the Rent and Sale toggle button
-        toggleRent.setOnClickListener(View.OnClickListener {
+        toggleRent.setOnClickListener {
             toggleSale.isChecked = !toggleSale.isChecked
-        })
+        }
         toggleSale.setOnClickListener(View.OnClickListener {
             toggleRent.isChecked = !toggleRent.isChecked
         })
