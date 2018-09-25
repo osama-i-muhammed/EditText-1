6c52232263f7ff020d0105e5e7c9b971ba7207a8
Salar Pro <salarkhalid97@yahoo.com>
2018-09-26 02:17:18 +0300

    Final2

--- app/src/main/java/com/duhok_tech/aqarat/MainMenu.kt
+++ app/src/main/java/com/duhok_tech/aqarat/MainMenu.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenu : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
