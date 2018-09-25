bf2117317ffc7368a8940c09e2fee6f0a6f6c108
Salar Pro <salarkhalid97@yahoo.com>
2018-09-25 21:14:51 +0300

    Add Icons Add MainMenu Add Filter Menu Add details Add Strings ...etc another thinks

--- app/src/main/java/com/duhok_tech/aqarat/InsertActivit/Add_Farm.kt
+++ app/src/main/java/com/duhok_tech/aqarat/InsertActivit/Add_Farm.kt
@@ -2,12 +2,22 @@ package com.duhok_tech.aqarat.InsertActivit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.duhok_tech.aqarat.R
+import kotlinx.android.synthetic.main.activity_add__farm.*
 
 class Add_Farm : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add__farm)
+
+        //check the Rent and Sale toggle button
+        toggleRent.setOnClickListener(View.OnClickListener {
+            toggleSale.isChecked = !toggleSale.isChecked
+        })
+        toggleSale.setOnClickListener(View.OnClickListener {
+            toggleRent.isChecked = !toggleRent.isChecked
+        })
     }
 }
--- app/src/main/java/com/duhok_tech/aqarat/MainMenu.kt
+++ app/src/main/java/com/duhok_tech/aqarat/MainMenu.kt
@@ -3,6 +3,7 @@ package com.duhok_tech.aqarat
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
 import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenu : AppCompatActivity() {
@@ -10,6 +11,8 @@ class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+//
+//        getWindow().setWindowAnimations(Animation.INFINITE)
 
         ssss.setOnClickListener {
             finish()
