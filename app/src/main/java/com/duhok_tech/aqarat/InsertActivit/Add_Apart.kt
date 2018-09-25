05e505b253b464bb353352df8c455e9a803a6cef
osama-i-muhammed <zduhoki93@gmail.com>
2018-09-25 15:01:57 +0300

    add new 7activit for inserting data

--- app/src/main/java/com/duhok_tech/aqarat/InsertActivit/Add_Apart.kt
+++ app/src/main/java/com/duhok_tech/aqarat/InsertActivit/Add_Apart.kt
@@ -9,5 +9,6 @@ class Add_Apart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add__apart)
+        //
     }
 }
